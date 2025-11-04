f37787c3c9abd6224092bec69860c1f6638a4b36
Patrick <patrickkenji@gmail.com>
2025-11-03 22:08:07 -0300

    fixing new errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react'; 
+import React, { Suspense, lazy } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import FeatureBar from './components/FeatureBar';
